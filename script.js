9bde019f590f8cc6db33940f74032bd50c803c29
Carl Citchen <carlcitchen@Jennifers-iMac.local>
2020-04-04 18:39:53 -0500

    push for Bruce

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 // Write your JavaScript code here!
 window.addEventListener("load", function() {
-   let form = document.querySelector("form");
-   form.addEventListener("submit", function(event) {
-         let pilotNameInput = document.querySelector("input[name=pilotName]");
-         let copilotNameInput = document.querySelector("input[name=copilotName]");
-         let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
-         let cargoMassInput = document.querySelector("input[name=cargoMass]");
-         if (pilotNameInput.value === "" || copilotNameInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === "" ) {
-            alert("All fields are required!");
-            event.preventDefault();
-         }
-         if (isNaN(fuelLevelInput.value)) {
-            alert("Make sure to enter valid information for each field!");
-            event.preventDefault();
-         } 
-         if (isNaN(cargoMassInput.value)){
-            alert("Make sure to enter valid information for each field!");
-            event.preventDefault();
-         }
-      });
-   });
-   let 
-
+   let form = document.getElementById("formSubmit");
+   let pilotNameInput = document.querySelector("input[name=pilotName]");
+   let copilotNameInput = document.querySelector("input[name=copilotName]");
+   let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+   let cargoMassInput = document.querySelector("input[name=cargoMass]");
+form.addEventListener("click", function() {
+   if (pilotNameInput.value === "" || copilotNameInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === "" ) {
+      alert("All fields are required!");
+      event.preventDefault();
+   }
+   if (isNaN(fuelLevelInput.value)) {
+      alert("Make sure to enter valid information for each field!");
+      event.preventDefault();
+   } 
+   if (isNaN(cargoMassInput.value)){
+      alert("Make sure to enter valid information for each field!");
+      event.preventDefault();
+   }
+});
+});
+   
+//document.getElementById("formSubmit").addEventListener("click", function() {
+   //alert("Hello World!");
+//});
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
